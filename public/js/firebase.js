04b25f385b327acaac81e908f7841eca0f4efc08
ptamayo <ptamayo97@yahoo.com>
2019-09-30 18:52:56 -0700

    added toasts

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,27 +1,69 @@
-import fbConfig from "../config/fbConfig";
-import firebase from "firebase";
-import "firebase/firestore";
-// // const firebase = require("firebase");
-// // Required for side-effects
-// require("firebase/firestore");
-firebase.initializeApp({ fbConfig });
+$(function() {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBZyQwVp6AvKXIs3gmFqjsImUVqGiFTYok",
+    authDomain: "portfolio-e31a9.firebaseapp.com",
+    databaseURL: "https://portfolio-e31a9.firebaseio.com",
+    projectId: "portfolio-e31a9",
+    storageBucket: "portfolio-e31a9.appspot.com",
+    messagingSenderId: "872417866321",
+    appId: "1:872417866321:web:7f7f65a4370c72e1cf7e70"
+  });
 
-const db = firebase.firestore();
+  var db = firebase.firestore();
 
-const submitContact = data => {
-  db.collection("contacts")
-    .add({
-      firstName: data.firstName,
-      lastName: data.lastName,
-      email: data.email,
-      message: data.message
-    })
-    .then(function(res) {
-      console.log(`Success: ${res.data}`);
-    })
-    .catch(function(error) {
-      console.error(`Error adding document: ${error}`);
-    });
-};
+  $("form[name='contactForm'").submit(event => {
+    const first = $("#firstName").hasClass("valid");
+    const last = $("#lastName").hasClass("valid");
+    const email = $("#email").hasClass("valid");
+    const message = $("#message").hasClass("valid");
+    event.preventDefault();
+    if (first && last && email && message) {
+      const firstName = $("#firstName")
+        .val()
+        .trim();
+      const lastName = $("#lastName")
+        .val()
+        .trim();
+      const email = $("#email")
+        .val()
+        .trim();
+      const message = $("#message")
+        .val()
+        .trim();
 
-export { db, submitContact };
+      const data = {
+        name: {
+          firstName: firstName,
+          lastName: lastName
+        },
+        email: email,
+        message: message
+      };
+
+      db.collection("contacts")
+        .add({
+          name: {
+            firstName: data.name.firstName,
+            lastName: data.name.lastName
+          },
+          email: data.email,
+          message: data.message
+        })
+        .then(() => {
+          console.log("Success!");
+          const success = "<span>Message Sent!</span>";
+          M.toast({ html: success, classes: "green darken-4" });
+        })
+        .catch(err => {
+          console.error(`Error adding document: ${err}`);
+          const error = "<span>Error!</span>";
+          M.toast({ html: error });
+        });
+
+      $("#firstName").val("");
+      $("#lastName").val("");
+      $("#email").val("");
+      $("#message").val("");
+    }
+  });
+});
